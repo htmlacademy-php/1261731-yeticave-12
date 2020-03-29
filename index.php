b8f7fa43870bc1e8d204819985350e91a3613e54
igor193php <iigor193@yandex.ru>
2020-03-29 19:01:46 +0000

    add if in index.php

--- index.php
+++ index.php
@@ -28,8 +28,21 @@ $user_name = 'Igor'; // укажите здесь ваше имя
 
         <nav class="user-menu">
 
-        <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
+	<!-- здесь должен быть PHP код для показа меню и данных пользователя -->
+<?php if ($is_auth == 1): ?>
+<div class="user-menu__logged">
+<p><?php echo $user_name ?></p>
+<a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+<a class="user-menu__logout" href="#">Выход</a>
+</div>
+<?php else: ?>
+<ul class="user-menu__list">
+<li class="user-menu__item">
+</li>
+<li class="user-menu__item">
+<a href="#">Вход</a>
+</li>
+</ul>
         </nav>
     </div>
 </header>
