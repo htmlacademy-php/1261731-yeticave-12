fca011688090c5834fb3281ae2ab85b0a62bb4b8
igor193php <iigor193@yandex.ru>
2020-04-05 14:35:32 +0000

    created databas

---
+++ schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE Users (
+    PRIMARY KEY (id),
+    id                INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name              CHAR (50)    NOT NULL,
+    email             CHAR (50)    NOT NULL,
+    password          CHAR (100)   NOT NULL,
+    contact           CHAR (255)   NOT NULL,
+    date_registration DATETIME     NOT NULL,
+                      UNIQUE (email)
+);
+CREATE INDEX password_idx ON Users (password);
+CREATE TABLE Categories (
+    PRIMARY KEY (id),
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name        CHAR (50),
+    symbol_code CHAR (50),
+                UNIQUE (name),
+                UNIQUE (symbol_code)
+);
+CREATE TABLE Rates (
+    PRIMARY KEY (id),
+    id          INT UNSIGNED          NOT NULL AUTO_INCREMENT,
+    user_id     INT UNSIGNED          NOT NULL,
+    lot_id      INT UNSIGNED          NOT NULL,
+    cost        DECIMAL (12, 2)       NOT NULL,
+    date_create DATETIME
+);
+CREATE TABLE Lots (
+    PRIMARY KEY (id),
+    id            INT UNSIGNED              NOT NULL AUTO_INCREMENT,
+    user_id       INT UNSIGNED              NOT NULL,
+    category_id   INT UNSIGNED              NOT NULL,
+    winner_id     INT UNSIGNED              NOT NULL,
+    name          CHAR (255)                NOT NULL,
+    detail        TEXT                      NOT NULL,
+    cost_start    DECIMAL (12, 2)           NOT NULL,
+    step_cost     DECIMAL (12, 2)           NOT NULL,
+    photo         CHAR (255),
+    date_create   DATETIME                  NOT NULL,
+    date_finished DATETIME                  NOT NULL,
+                  FOREIGN KEY (user_id)     REFERENCES Users (id),
+                  FOREIGN KEY (category_id) REFERENCES Categories (id),
+                  FOREIGN KEY (winner_id)   REFERENCES Users (id)
+);
+CREATE INDEX name_idx ON Lots (name);
+ALTER TABLE Rates ADD FOREIGN KEY (user_id) REFERENCES Users (id);
+ALTER TABLE Rates ADD FOREIGN KEY (lot_id) REFERENCES Lots (id);
+
+
